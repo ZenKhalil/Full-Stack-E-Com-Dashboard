0c8079bec868361dcad832e10a21af441e91ce82
ZenKhalil <feattzen@gmail.com>
2024-01-12 21:59:48 +0100

    Implemented the modal, dialog, and store modal dependencies

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,6 +1,24 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (<p>Hello admin dashboard</p>
-)
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+  
+useEffect(() => {
+  if (!isOpen) {
+    onOpen();
+  }
+}, [isOpen, onOpen]);
+
+  return (
+    <div className="p-4">
+        Root Page
+    </div>
+  );
 }
+
+export default SetupPage;
